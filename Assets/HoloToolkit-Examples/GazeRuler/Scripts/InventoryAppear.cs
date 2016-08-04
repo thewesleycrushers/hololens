27ed74e3d455e94a683d2dffa0e00a8404d9acab
Pascal Crenzin <pascalslogin@gmail.com>
2016-08-04 08:38:07 -0700

    not working speechmanager

---
+++ Assets/HoloToolkit-Examples/GazeRuler/Scripts/InventoryAppear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryAppear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        GetComponent<Renderer>().enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //called by SpeechManager
+    void onInventory()
+    {
+        GetComponent<Renderer>().enabled = true;
+    }
+}
